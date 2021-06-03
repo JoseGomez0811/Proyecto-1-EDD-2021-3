c7dc12953a9bacb2ceab07614bd8a573a01bcb0a
José Gómez <gomez.jose@correo.unimet.edu.ve>
2021-06-03 07:51:08 -0400

    Algunos arreglos en clase Nodo

--- OrganizedChaos/src/Clases/Nodo.java
+++ OrganizedChaos/src/Clases/Nodo.java
@@ -13,10 +13,10 @@ public class Nodo {
     public Object dato;
     Nodo pNext;
     
-}/**
-*Constructor de la clase Nodo
-* @param dato El parámetro dato define el elemento al cual se le hará referencia en la lista
-**/
+    /**
+    *Constructor de la clase Nodo
+    * @param dato El parámetro dato define el elemento al cual se le hará referencia en la lista
+    **/
     public Nodo(Object dato) {
         this.dato = dato;
         pNext= null;
